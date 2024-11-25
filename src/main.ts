205ffe85969cd50db89f8b33762add79198f07bd
yonghanjung <dydgks789@naver.com>
2024-11-25 10:24:36 +0900

    feat(bootstrap) - 부트스트랩 라이브러리 설치

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from "vue";
 import "./assets/style.css";
 import App from "./App.vue";
 import router from "./router";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const app = createApp(App);
+
 app.use(router).mount("#app");
