68db078ade661d7568f9099c119757c1aaa6cc34
yonghanjung <dydgks789@naver.com>
2024-11-25 10:31:12 +0900

    feat(store) - 전역 상태 관리를 위한 pinia 설치

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,10 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import PrimeVue from "primevue/config";
 import Aura from "@primevue/themes/aura";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
+const pinia = createPinia();
 
 app
   .use(router)
@@ -16,4 +18,5 @@ app
       preset: Aura,
     },
   })
+  .use(pinia)
   .mount("#app");
