8608ba6173824f0eb8475ddb3b3ac4ecd1b3634f
yonghanjung <dydgks789@naver.com>
2024-11-25 10:28:00 +0900

    feat(primevue) - primevue 라이브러리 설치

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import App from "./App.vue";
 import router from "./router";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
 
 const app = createApp(App);
 
-app.use(router).mount("#app");
+app
+  .use(router)
+  .use(PrimeVue, {
+    theme: {
+      preset: Aura,
+    },
+  })
+  .mount("#app");
